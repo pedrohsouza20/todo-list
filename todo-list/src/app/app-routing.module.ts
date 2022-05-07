a4d73663f8ab6188e07f3795155deb836001f93e
Pedro <contato.pedromoc@gmail.com>
2022-05-07 19:19:48 -0300

    iniciadas configurações de rotas front-end / modificada arquitetura do projeto

---
+++ todo-list/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './components/pages/auth/login/login.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
